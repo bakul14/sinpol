4e1f20f1f63912fee4daa18ad95aac04fbfd246a
bakul14 <m_bakulevskiy@baumanracing.ru>
2024-09-17 14:49:05 +0300

    (+) empty src files have been created

---
+++ sinpol.srcs/sources_1/new/sinpol.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 17.09.2024 14:44:56
+// Design Name: 
+// Module Name: sinpol
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sinpol(
+
+    );
+endmodule
